d1df7a57f8107f60bf392a6a169db2a5cde3f9b3
Ankit Nagdeve <83922506+ankit-nagdeve@users.noreply.github.com>
2025-06-07 02:52:53 +0530

    Add files via upload

---
+++ prompts/base.py
@@ -0,0 +1,3 @@
+
+GREETING_PROMPT = "👋 Hello! I’m TalentScout, your virtual hiring assistant. I’ll help gather your info and ask relevant questions based on your skills."
+ENDING_KEYWORDS = ["bye", "exit", "thank you", "end", "quit"]
